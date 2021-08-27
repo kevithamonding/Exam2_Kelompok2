a8c599f92df300949adf3974fb72e2a5cf1ef382
kevithamonding <kevithapraisy@gmail.com>
2021-08-27 20:08:04 +0800

    Menambahkan Main Menu pada MBKM_JDBC.java

--- MBKM_JDBC/src/tools/MBKM_JDBC.java
+++ MBKM_JDBC/src/tools/MBKM_JDBC.java
@@ -19,9 +19,30 @@ public class MBKM_JDBC {
     /**
      * @param args the command line arguments
      */
+    Scanner sc = new Scanner(System.in);
+    public int pil;
+
+    public void Menu() {
+        System.out.println("================== MENU CRUD ==================");
+        System.out.println("1. Insert");
+        System.out.println("2. Update");
+        System.out.println("3. Delete");
+        System.out.println("4. View All");
+        System.out.println("5. Search by Id");
+        System.out.println("6. Back to Main Menu");
+        System.out.println("===============================================");
+        System.out.print("Masukkan pilihan : ");
+        this.pil = sc.nextInt();
+        System.out.println("===============================================");
+
+    }
+
     public static void main(String[] args) {
+
         DBConnection dbc = new DBConnection();
         Scanner sc = new Scanner(System.in);
+
+        int piltabel;
 
         //Test Connection
         System.out.println(dbc.getConnection());
@@ -29,7 +50,36 @@ public class MBKM_JDBC {
         //Dependency Injection
         RegionDAO rdao = new RegionDAO(dbc.getConnection());
 
-            
+        do {
+            System.out.println("================= MENU UTAMA =================");
+            System.out.println("1. REGION");
+            System.out.println("2. COUNTRY");
+            System.out.println("3. LOCATION");
+            System.out.println("4. DEPARTMENT");
+            System.out.println("5. JOB");
+            System.out.println("6. EMPLOYEE");
+            System.out.println("7. Exit");
+            System.out.println("==============================================");
+            System.out.print("Masukkan pilihan : ");
+            piltabel = sc.nextInt();
+            System.out.println("==============================================");
+
+            switch (piltabel) {
+                case 1:
+                    RegionView region = new RegionView();
+                    region.Logika();
+                    break;
+                case 2:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        } while (piltabel != 7);
+
     }
 
 }
